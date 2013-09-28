f2aed0480f0bf9ee2697974c1d7a113e449f21dd
Nash Kaminski <nashkaminski@comcast.net>
2013-09-27 20:04:29 -0500

    progress..

--- real_deal/arm/arm.ino
+++ real_deal/arm/arm.ino
@@ -9,13 +9,15 @@ Control Bits:
 
 */
 uint8_t control_bits;
-uint16_t ticks;
+uint16_t ticks=0,releasetick;
 uint8_t getControlBit(uint8_t i){
   return (control_bits >> i)&0x01;
 }
-void setControlBit(uint8_t i, uint8_t v){
-  control_bits &= ~(1<<i);
-  control_bits |= (v<<i);
+void setControlBit(uint8_t i){
+  control_bits |= (1<<(i&0x01));
+}
+void clearControlBit(uint8_t i){
+control_bits &= ~(1<<(i&0x01));
 }
 void ISR_Z(){
   //We have a fire command! Attach the A_Interrupt!
@@ -25,6 +27,13 @@ void ISR_Z(){
 }
 void ISR_A(){
   ticks++;
+  if(ticks >= releasetick){
+    fire();
+    setControlBit(3);
+  }
+}
+void fire(){
+ //stuff to actually fire goes here 
 }
 void setup(){
   attachInterrupt(Z_INT,ISR_Z,RISING);
