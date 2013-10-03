6ab12939d49ca7fdd3ad005d71e26fabe1c2d8c1
Nash Kaminski <nashkaminski@comcast.net>
2013-10-02 22:38:42 -0500

    Major progress on arm code

--- real_deal/arm/arm.ino
+++ real_deal/arm/arm.ino
@@ -1,48 +1,75 @@
+#define SerComm Serial1
+#define TX_INT 50
 #define A_INT 0
 #define Z_INT 1
+#define _GCB(b) (control_bits & (1 << (b)))
+#define _CCB(b) (control_bits &= ~(1<<((b) & 0x01)));
+#define _SCB(b) (control_bits |= (1<<((b) & 0x01)));
 /*
 Control Bits:
-0: Fire command issued
-1: ISR_A registration confirmed
-2: Release enable
-3: Release mechanism triggered
+ 0: Fire command issued
+ 1: ISR_A registration and calculations confirmed
+ 2: Release enable
+ 3: Release mechanism triggered
+ 
+ */
+uint8_t control_bits=0;
+uint16_t ticks,releasetick;
+long period=millis(),txtime=millis();
+int rpm;
+int calcTick(){
+  //Calculate and return tick at which release is to occur at 
 
-*/
-uint8_t control_bits;
-uint16_t ticks=0,releasetick;
-uint8_t getControlBit(uint8_t i){
-  return (control_bits >> i)&0x01;
 }
-void setControlBit(uint8_t i){
-  control_bits |= (1<<(i&0x01));
-}
-void clearControlBit(uint8_t i){
-control_bits &= ~(1<<(i&0x01));
+void fire(){
+  //stuff to actually fire goes here 
+
 }
 void ISR_Z(){
-  //We have a fire command! Attach the A_Interrupt!
-  if(getControlBit(0)&&(!getControlBit(1))){
-    attachInterrupt(A_INT,ISR_A,RISING);
-  }
+  rpm=1/((millis()-period)/1000)/60;
+  period=millis();
+  if(_GCB(1)){
+    //Launch parameters are ready! Enable Release!
+    ticks=0;
+    _SCB(2)
+    }
+    //We have a fire command! Attach the A_Interrupt!
+    if(_GCB(0)&&(!_GCB(1))){
+      attachInterrupt(A_INT,ISR_A,RISING);
+      releasetick=calcTick();
+      _SCB(1);
+    }
 }
 void ISR_A(){
   ticks++;
-  if(ticks >= releasetick){
+  if((ticks >= releasetick) && _GCB(2)){
     fire();
-    setControlBit(3);
+    _SCB(3);
+    detachInterrupt(A_INT);
+    _CCB(2);
+    _CCB(1);
   }
 }
-void fire(){
- //stuff to actually fire goes here 
-}
+
 void setup(){
   attachInterrupt(Z_INT,ISR_Z,RISING);
+#ifdef DEBUG
+  SerComm.begin(57600);
+#endif
 }
 void loop(){
-  //Detatch interrupt if fire command bit is cleared
-  if(!getControlBit(0)){
-         detachInterrupt(A_INT);
+  if((millis()-txtime)>TX_INT){
+    SerComm.print('S');
+    SerComm.write(control_bits);
+    SerComm.println(rpm);
+    txtime=millis();
+  }
+  if(SerComm.available()&&!_GCB(0)){
+    if(SerComm.read()=='F'){
+      _SCB(0);
+    }
   }
 }
-  
-  
+
+
+
